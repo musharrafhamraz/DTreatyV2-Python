12dc1919ee8e528896a1a73d6992ca09f79d4446
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-02-23 12:05:33 +0500

    new screen added

--- dtreaty.py
+++ dtreaty.py
@@ -43,7 +43,7 @@ class Dtreaty(MDApp):
                     pil_image = pil_image.resize((224, 224))  # Resize the image to the desired input size
                     image_array = img_to_array(pil_image)
                     image_array = image_array / 255.0
-                    self.model = tf.keras.models.load_model('DTreatyVGG19.h5')
+                    self.model = tf.keras.models.load_model('treatment-book.csv')
                     prediction = self.model.predict(np.expand_dims(image_array, axis=0))
                     predicted_class = np.argmax(prediction, axis=1)
                     class_names = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
